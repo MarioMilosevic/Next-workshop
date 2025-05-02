244920bcc84e69c4942f310b4202394920a9fbcf
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2025-05-02 10:31:11 +0200

    Finish exercise component

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <h1>Ovo je neki naslov</h1>
+      <h2>ovo je podnaslov</h2>
+    </>
+  );
+};
+
+export default Header;
